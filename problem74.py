f10edf9c22ed8aede4d669bc85c145ceec9f90a8
Manuel René Pauls Toews <paulsmrene@live.com>
2024-11-06 08:32:37 -0300

    Add problems (solved)

--- problem74.py
+++ problem74.py
@@ -9,13 +9,11 @@ known = {
     "45361": 2,
     "872": 2,
     "45362": 2,
-    "1": 1,
 }
 
 for k, v in known.items():
-    dp[int(k)] = v -1
     k = "".join(sorted(k.replace("0", "1")))
-    dp[k] = v
+    dp[k] = v +1
 
 f = [0] * 10
 for i in range(1, 10):
@@ -24,7 +22,6 @@ for i in range(1, 10):
 sixty_long_count = 0
 max_chain = 0
 for i in reversed(range(1_000_000)):
-# for i in range(69,69+1):
     starts = "".join(sorted(str(i).replace("0", "1")))
     s = starts
     
@@ -32,35 +29,38 @@ for i in reversed(range(1_000_000)):
         if dp[starts] == 60:
             sixty_long_count += 1
         continue
-    dp[starts] = 0
+    this_chain = 0
 
     prev = i
     while True:
-        dp[starts] += 1
+        this_chain += 1
         next = 0
         for d in s:
             next += f[int(d)]
         s = "".join(sorted(str(next).replace("0", "1")))
-        print(next)
+        # print(next)
         if next == prev:
             break
         if s in dp:
-            dp[starts] += dp[s]
-            print("found", s, dp[s])
+            if  str(next) in known:
+                this_chain -= 1
+            this_chain += dp[s]
+            # print("found", s, dp[s])
             break
         prev = next
 
-    if dp[starts] == 60:
+    if this_chain == 60:
         sixty_long_count += 1
-        print(i, dp[starts])
+        # print(f"{i} is 60 long")
+    if starts not in dp:
+        dp[starts] = this_chain
     if dp[starts] > max_chain:
-        print(i)
+        # print(i)
         max_chain = dp[starts]
-    dp[i] = dp[starts] -1
 
-    print(i, dp[starts])
-    print()
-    print()
+    # print(i, dp[starts])
+    # print()
+    # print()
 
 print(sixty_long_count)
 print(max_chain)
